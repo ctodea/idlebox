fda0e52593b67946f7691741b0398756226005c6
Cristian Todea <ctodea@users.noreply.github.com>
2018-03-23 00:37:10 +0100

    readability++

--- code/fizzbuzz.sh
+++ code/fizzbuzz.sh
@@ -2,10 +2,10 @@
 
 #http://wiki.c2.com/?FizzBuzzTest
 
-for x in {1..100}; do 
-  (($x%3 == 0)) && e="Fizz" 
-  ((${x}%5 == 0)) && e="${e}Buzz"
-  echo "${e:-$x}"
+for i in {1..100}; do
   e=""
+  (( $i % 3 == 0 )) && e="Fizz" 
+  (( $i % 5 == 0 )) && e="${e}Buzz"
+  echo ${e:-$i}
 done
 
