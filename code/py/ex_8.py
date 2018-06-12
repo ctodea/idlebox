73b99808ac86e372cb1e70d1911f49c37a0d8859
Cristian Todea <todea94@gmail.com>
2018-06-12 09:32:46 +0100

    while True + anotherGame

--- code/py/ex_8.py
+++ code/py/ex_8.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 #inspired in https://www.practicepython.org/exercise/2014/03/26/08-rock-paper-scissors.html
 import sys
+import re
+
 
 def selection(player):
     print("Player ", player,", select:", sep='')
@@ -9,6 +11,7 @@ def selection(player):
     print("3: Scissors")
     move = input("Select your move: ")
     return move
+
 
 #0 = draw, 1 = player 1 win, 2 = player 2 win
 def rules(x, y):
@@ -20,13 +23,25 @@ def rules(x, y):
     elif x == 3 and y == 2: return 1
     else: return 2
 
-def game():
-    
-    p1 = selection(1)
-    p2 = selection(2)
-    # print("player1:", p1)
-    # print("player2:", p2)
-    winner = rules(p1, p2)
-    print("Player ", winner, " wins", sep='')
 
+def anotherGame():
+    for retries in range(3):
+        flag = input("Do you want to play another game? (Yy/Nn): ")
+        if re.match("^[YyNn]{1}$", flag):
+            if flag == 'Y' or flag == 'y':
+                return True
+            else: return False
+        elif retries == 2: 
+            print("Bye")
+            return False
+
+
+def game():
+    while True:
+        p1 = selection(1)
+        p2 = selection(2)
+        winner = rules(p1, p2)
+        if winner is 0: print("Draw")
+        else: print("Player ", winner, " wins", sep='')
+        if anotherGame() is False: break
 game()
