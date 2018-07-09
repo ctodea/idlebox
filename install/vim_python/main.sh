a532399802bf053dc13480628e6962fb69f19992
Cristian Todea <todea94@gmail.com>
2018-07-09 15:01:28 +0100

    deleted unused script

--- install/vim_python/main.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#Installation command
-cmd="sudo apt-get install -y"
-pre_cmd="sudo apt-get update"
-pkgs="python3-pip vim git"
-
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
