2d09efed9028d286f1b8bb7885dd4494e993d2c0
Cristian Todea <todea94@gmail.com>
2018-07-12 19:10:39 +0100

    simple aws sample

---
+++ code/hcl/aws/templates/main.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+  region = "eu-west-2"
+}
+
+resource "aws_instance" "foo" {
+  ami             = "ami-b8b45ddf"
+  instance_type   = "t2.micro"
+  security_groups = ["${aws_security_group.allow_ssh_access.name}"]
+}
+
+resource "aws_security_group" "allow_ssh_access" {
+  name        = "ssh_22"
+  description = "Allow ingress traffic to port 22 for enabling ssh connection"
+
+  ingress {
+    from_port   = 0
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags {
+    tier = "dev"
+  }
+}
