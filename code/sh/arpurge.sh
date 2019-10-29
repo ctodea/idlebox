19759d8767141a4d778093e356d08edabb640a0b
Cristian Todea <ctodea@users.noreply.github.com>
2019-10-29 10:17:31 +0000

    Update arpurge.sh

--- code/sh/arpurge.sh
+++ code/sh/arpurge.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-INVENTORY_FILE="/home/caocpusr/inv"
-LOG_FILE="/home/caocpusr/log"
+INVENTORY_FILE="~/inv"
+LOG_FILE="~/log"
 for i in $(cat "${INVENTORY_FILE}"); do
     INCOMPLETE= && INCOMPLETE=$(ssh -q $i '. /etc/profile; sudo arp -a | grep eth0 | grep incomplete' | grep -v '?' )
     [[ -n $INCOMPLETE ]] && echo $(date '+%Y-%m-%d %H:%M:%S') NODE $i has imcomplete entries: "$INCOMPLETE" >> $LOG_FILE && echo >> $LOG_FILE #some loging
