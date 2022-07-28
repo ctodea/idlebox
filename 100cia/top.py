ebbf098a5ba49edef107d9f91d245b1cda86f29b
Cristian Todea <cristian.todea@cloudwerkstatt.com>
2022-07-29 00:52:30 +0200

    Add third order process calculation

---
+++ 100cia/top.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+from itertools import combinations_with_replacement
+import numpy as np
+
+wave_length = []
+c = 3*10**17  # m/s
+n = int(input("Enter number of elements: "))
+target = c/640
+# target = 10
+# iterating till the range
+for i in range(0, n):
+    ele = int(input("Input value:"))
+
+    wave_length.append(ele) # adding the element
+    wave_length.append(-ele)
+
+freqs = [c/x for x in wave_length]
+
+a = 0
+b = len(freqs) - 1
+
+combs = ([comb for comb in combinations_with_replacement(freqs, 3)])
+sums = np.array([sum(i) for i in combs])
+
+# print(combs)
+# print(sums)
+
+difference_array = np.absolute(sums-target)
+index = difference_array.argmin()
+print(combs[index])
+print(sums[index])
+print(target)
