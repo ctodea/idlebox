497561bc504dedc9fe0841061926d6bdaaa1c212
Cristian Todea <cristian.todea@cloudwerkstatt.com>
2022-07-29 00:54:52 +0200

    Remove unused vars

--- 100cia/top.py
+++ 100cia/top.py
@@ -3,21 +3,17 @@ from itertools import combinations_with_replacement
 import numpy as np
 
 wave_length = []
-c = 3*10**17  # m/s
+c = 3*10**17  # nm/s
 n = int(input("Enter number of elements: "))
 target = c/640
 # target = 10
 # iterating till the range
 for i in range(0, n):
     ele = int(input("Input value:"))
-
-    wave_length.append(ele) # adding the element
-    wave_length.append(-ele)
+    wave_length.append(ele)  # adding the element
+    wave_length.append(-ele)  # unefficiently calculation subs too
 
 freqs = [c/x for x in wave_length]
-
-a = 0
-b = len(freqs) - 1
 
 combs = ([comb for comb in combinations_with_replacement(freqs, 3)])
 sums = np.array([sum(i) for i in combs])
