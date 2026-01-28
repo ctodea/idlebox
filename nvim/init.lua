6b58495f1d45c81f16c198b373fce28876e59e45
Cristian Todea <todea@hardstaq.com>
2026-01-28 21:18:32 +0100

    Add configs
    More custom keybinds, rustfmt + indenting. And more

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,5 @@
 require("ctodea")
+
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "rust",
   callback = function()
@@ -6,6 +7,16 @@ vim.api.nvim_create_autocmd("FileType", {
       name = "rust-analyzer",
       cmd = { "rust-analyzer" },
       root_dir = vim.fs.dirname(vim.fs.find({ "Cargo.toml" }, { upward = true })[1]),
+      settings = {
+        ['rust-analyzer'] = {
+          check = {
+            command = "clippy",
+          },
+          diagnostics = {
+            enable = true,
+          },
+        },
+      },
     })
   end,
 })
@@ -18,5 +29,34 @@ vim.api.nvim_create_autocmd("LspAttach", {
     vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
     vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
     vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+    
+    -- Format and indent with Shift-Tab
+    vim.keymap.set('n', '<S-Tab>', function()
+      local pos = vim.api.nvim_win_get_cursor(0)
+      vim.cmd('normal! gg=G')
+      vim.lsp.buf.format({ async = false })
+      vim.api.nvim_win_set_cursor(0, pos)
+    end, opts)
   end,
 })
+
+vim.opt.clipboard = 'unnamedplus'
+
+vim.diagnostic.config({
+  virtual_text = true,
+  signs = true,
+  underline = true,
+  update_in_insert = true,
+  severity_sort = true,
+})
+
+-- Inlay hint colors
+vim.api.nvim_set_hl(0, 'LspInlayHint', {
+  fg = '#fff700',
+  italic = true,
+})
+
+-- Toggle inlay hints manually
+vim.keymap.set('n', '<leader>h', function()
+  vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+end, { desc = 'Toggle inlay hints' })
--- nvim/lua/ctodea/remap.lua
+++ nvim/lua/ctodea/remap.lua
@@ -1,2 +1,4 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>;", "$a;<Esc>")
+vim.keymap.set('n', '<S-Tab>', 'gg=G<C-o>', { desc = 'Indent whole file' })
